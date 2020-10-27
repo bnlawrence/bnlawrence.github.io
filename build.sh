f0077afb64cbc9adfd06e6adb5978f2effbae7b6
Bryan Lawrence <bryannlawrence@gmail.com>
2020-10-27 09:20:34 +0000

    Humans in climate

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-./build4deploy.sh
-./distribute.sh
+JEKYLL_ENV=production jekyll build --config _config-deploy.yml 
+rsync -v --recursive --exclude='.git/' --filter "protect ./git" --delete-after _site/ ../github-deployed
+cd ../github-deployed
+git add --all *
