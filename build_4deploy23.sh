709e16af493be9b19d8cfc53a88dea76f61e4ea4
Bryan Lawrence <b.n.lawrence@reading.ac.uk>
2023-06-20 06:41:43 +0100

    Facelifted

--- build_4deploy23.sh
+++ build_4deploy23.sh
@@ -1,5 +1,8 @@
+
+# Make sure the existing container is stopped, you need a new
+# container to ensure you rebuild with the deployment config
 export JEKYLL_VERSION=3.8
-docker run --rm \
+docker run --name bjekyll --rm \
   --volume="$PWD:/srv/jekyll" \
   --volume="$PWD/../usr_local_bundle:/usr/local/bundle" \
   -it jekyll/jekyll:$JEKYLL_VERSION \
@@ -7,4 +10,3 @@ docker run --rm \
 rsync -v --recursive --exclude='.git/' --filter "protect ./git" --delete-after _site/ ../github-deployed
 cd ../github-deployed
 git add --all *
-
